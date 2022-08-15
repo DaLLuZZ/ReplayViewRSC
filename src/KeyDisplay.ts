21f47866e6f9cfa84de1b0b160bfb5b7b922af34
DaLLuZZ <79600653+DaLLuZZ@users.noreply.github.com>
2022-08-16 00:26:57 +0300

    Fix velocity vector init

--- src/KeyDisplay.ts
+++ src/KeyDisplay.ts
@@ -131,7 +131,7 @@ namespace ReplayViewRSC {
             const replay = this.viewer.replay;
 
             var tickData;
-            var velocity;
+            var velocity = new Facepunch.Vector3();
 
             replay.getTickData(tick, tickData);
             velocity.copy(tickData.velocity);
