4354396a77b3717953d606ec59dfd7f13f73761d
DaLLuZZ <khrushiov2012@yandex.ru>
2022-08-30 18:09:39 +0000

    Automatic JS update

--- js/sourceutils.js
+++ js/sourceutils.js
@@ -1191,6 +1191,9 @@ var SourceUtils;
             else {
                 sample = samples[0];
             }
+            // DaLLuZZ: HACK FIX, TODO: CHECK surf_calzone models (ramps) loading
+            if (sample == null)
+                return 0x7f7f7f;
             mul = normal.x * normal.x;
             rgb.add(sample.x * mul, sample.y * mul, sample.z * mul);
             if (normal.y < 0) {
@@ -1199,9 +1202,6 @@ var SourceUtils;
             else {
                 sample = samples[2];
             }
-            // DaLLuZZ: HACK FIX, TODO: CHECK surf_calzone models (ramps) loading
-            if (sample == null)
-                return 0x7f7f7f;
             mul = normal.y * normal.y;
             rgb.add(sample.x * mul, sample.y * mul, sample.z * mul);
             if (normal.z < 0) {
